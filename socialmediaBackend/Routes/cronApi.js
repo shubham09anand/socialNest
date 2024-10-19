21c322515b81672722f99ef84f51ad8ade90394e
shubham09anand <shubhamsheelam@gmail.com>
2024-10-19 22:13:45 +0530

    init

--- socialmediaBackend/Routes/cronApi.js
+++ socialmediaBackend/Routes/cronApi.js
@@ -8,7 +8,8 @@ const scheduleCronJob = () => {
   cron.schedule('* * * * *', async () => {
     try {
       const response = await axios.post(`http://192.168.1.7:8080/auth/processScheduledMessage`);
-      // const response = await axios.post(`${process.env.REACT_APP_BASE_URL_NETWORK}/auth/processScheduledMessage`);
+
+      // const response = await axios.post(`https://apisocialnest.shubham09anand.in/auth/processScheduledMessage`);
       console.log('Cron job executed successfully:', response.data);
     } catch (error) {
       console.error('Error executing cron job:', error.message || error);
