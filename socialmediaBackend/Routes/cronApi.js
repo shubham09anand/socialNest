de1bd735e9acda25e9fb8a50af26b39adc5e7429
shubham09anand <shubhamsheelam@gmail.com>
2024-10-20 21:04:44 +0530

    init

--- socialmediaBackend/Routes/cronApi.js
+++ socialmediaBackend/Routes/cronApi.js
@@ -7,9 +7,9 @@ const scheduleCronJob = () => {
   // Schedule a job to run every minute (adjust as needed)
   cron.schedule('* * * * *', async () => {
     try {
-      const response = await axios.post(`http://192.168.1.7:8080/auth/processScheduledMessage`);
+      // const response = await axios.post(`http://192.168.1.7:8080/auth/processScheduledMessage`);
 
-      // const response = await axios.post(`https://apisocialnest.shubham09anand.in/auth/processScheduledMessage`);
+      const response = await axios.post(`https://apisocialnest.shubham09anand.in/auth/processScheduledMessage`);
       console.log('Cron job executed successfully:', response.data);
     } catch (error) {
       console.error('Error executing cron job:', error.message || error);
--- socialmediaBackend/server.js
+++ socialmediaBackend/server.js
@@ -14,8 +14,8 @@ connectDB();
 
 const app = express();
 app.use(cors({
-    // origin: [process.env.REACT_APP_API_SOCKET_NETWORK, process.env.REACT_APP_BASE_SOCKET_NETWORK, 'http://socialnest.shubham09anand.in', 'http://13.202.210.238:3000', 'https://socialnest.shubham09anand.in'],
-    origin: [process.env.REACT_APP_API_SOCKET_NETWORK, process.env.REACT_APP_BASE_SOCKET_NETWORK],
+    origin: ['http://socialnest.shubham09anand.in', 'http://13.202.210.238:3000', 'https://socialnest.shubham09anand.in'],
+    // origin: [process.env.REACT_APP_API_SOCKET_NETWORK, process.env.REACT_APP_BASE_SOCKET_NETWORK],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     optionsSuccessStatus: 200  
 }));
@@ -32,8 +32,8 @@ app.use(express.urlencoded({ extended: true }));
 const io = new Server(server, {
     maxHttpBufferSize: 12 * 1024 * 1024,
     cors: {
-        // origin: [process.env.REACT_APP_API_SOCKET_NETWORK, process.env.REACT_APP_BASE_SOCKET_NETWORK, 'http://13.202.210.238:3000', 'http://socialnest.shubham09anand.in:3000', 'https://socialnest.shubham09anand.in'],
-        origin: [process.env.REACT_APP_API_SOCKET_NETWORK, process.env.REACT_APP_BASE_SOCKET_NETWORK],
+        origin: ['http://13.202.210.238:3000', 'http://socialnest.shubham09anand.in:3000', 'https://socialnest.shubham09anand.in'],
+        // origin: [process.env.REACT_APP_API_SOCKET_NETWORK, process.env.REACT_APP_BASE_SOCKET_NETWORK],
         methods: ['GET', 'POST'],
         credentials: true
     }
