ffcc3405b5ca496f50c4c14a3138ef8efe2428ca
shubham09anand <shubhamsheelam@gmail.com>
2024-10-22 17:27:37 +0530

    init

--- socialmedia/src/Components/SearchedPerson/SearchedPerosn.jsx
+++ socialmedia/src/Components/SearchedPerson/SearchedPerosn.jsx
@@ -59,7 +59,7 @@ const SearchedPerson = () => {
 
   return (
     <>
-      <div className={`${loggedUser === null ? 'pb-20 w-full mx-auto shadow-2xl h-fit mt-3' : 'pb-20 lg:absolute right-0 w-full lg:w-[80%] lg:p-5'}`}>
+      <div className={`${loggedUser === null ? 'pb-20 w-full mx-auto shadow-2xl h-fit' : 'pb-20 lg:absolute right-0 w-full lg:w-[80%] lg:p-5 mt-3'}`}>
         <div className={`w-full bg-white lg:rounded-b-2xl ${loggedUser === null ? "lg:mt-14" : 'lg:-mt-10' }`}>
           <div className="relative overflow-hidden w-full lg:h-72 h-36 md:h-48">
             {UserData?.userProfile2?.backGroundPhoto || UserData?.userProfile2?.backGroundPhoto === "" ?
