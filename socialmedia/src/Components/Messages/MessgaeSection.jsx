29a38980f0b114dd24c71d8af543e5d00f3d2cfe
shubham09anand <shubhamsheelam@gmail.com>
2024-11-27 15:26:43 +0530

    init

--- socialmedia/src/Components/Messages/MessgaeSection.jsx
+++ socialmedia/src/Components/Messages/MessgaeSection.jsx
@@ -71,16 +71,16 @@ const MessgaeSection = ({ userPhoto }) => {
         const res = await API.post("/deleteMessage", { messageID });
         try {
             if (res.status === 200 && res.data.deleteCount === 1) {
-               
-                    setDelteMessage(null);
-                    setMessages(Messages.filter((_, i) => i !== index));
+
+                setDelteMessage(null);
+                setMessages(Messages.filter((_, i) => i !== index));
             } else if (res.status === 200 && res.data.deleteCount === 0) {
                 toast.error("Something went wrong")
             }
         } catch (error) {
             toast.error("Something went wrong")
-        }finally{
-                setDeleteStatus(false);
+        } finally {
+            setDeleteStatus(false);
         }
     };
 
@@ -106,18 +106,23 @@ const MessgaeSection = ({ userPhoto }) => {
                                     <div className="flex flex-col space-y-2">
                                         {msg.messagePhoto.length > 0 && (
                                             <div className='flex-col relative items-start w-fit h-full mx-auto space-y-3'>
-                                                {msg.messagePhoto.map((_, index) => (
-                                                    <div key={index} className="relative overflow-hidden max-w-lg border rounded-xl border-gray-200">
-                                                        <svg onClick={() => handleDownloadImage(msg.messagePhoto[index]?.base64, msg.messagePhoto[index]?.name)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="white" className="size-6 absolute rounded-md opacity-90 right-4 top-2 bg-[#6e8ee1] p-1 cursor-pointer">
+                                                {msg.messagePhoto.map((_, photoIndex) => (
+                                                    <div key={photoIndex} className="relative overflow-hidden max-w-lg border rounded-xl border-gray-200">
+                                                        {msg.sourceId === sender_id && (
+                                                            <svg onClick={() => handleDelteMessage(msg?._id, index)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white" className="size-6 absolute rounded-md opacity-90 right-12 top-2 bg-[#6e8ee1] p-1 cursor-pointer">
+                                                                <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                                            </svg>
+                                                        )}
+                                                        <svg onClick={() => handleDownloadImage(msg.messagePhoto[photoIndex]?.base64, msg.messagePhoto[photoIndex]?.name)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="white" className="size-6 absolute rounded-md opacity-90 right-2 top-2 bg-[#6e8ee1] p-1 cursor-pointer">
                                                             <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
                                                         </svg>
-                                                        <img className="h-auto w-80 sm:max-w-96 max-h-40 object-cover" src={msg.messagePhoto[index]?.base64 || postImagErr} onError={(e) => e.target.src = postImagErr} alt={`imgErr-${index}`} />
+                                                        <img className="h-auto w-80 sm:max-w-96 max-h-40 object-cover" src={msg.messagePhoto[photoIndex]?.base64 || postImagErr} onError={(e) => e.target.src = postImagErr} alt={`imgErr-${photoIndex}`} />
                                                     </div>
                                                 ))}
                                             </div>
                                         )}
                                         {delteMessage === index && (
-                                            <div onClick={()=> handleDelteMessage(msg?._id,index)} className='w-full flex flex-row-reverse'>
+                                            <div onClick={() => handleDelteMessage(msg?._id, index)} className='w-full flex flex-row-reverse'>
                                                 <div className={`flex text-center bg-gray-200 cursor-pointer select-none border-[0.1px] border-gray-900 text-gray-800 px-2 py-1 rounded-lg`}>{deleteStatus ? 'Deleting' : 'Delete'}</div>
                                             </div>
                                         )}
@@ -127,7 +132,7 @@ const MessgaeSection = ({ userPhoto }) => {
                                                 {msg.sourceId === sender_id && (
                                                     <>
                                                         <svg onClick={() => delteMessage === null ? setDelteMessage(index) : delteMessage === index ? setDelteMessage(null) : setDelteMessage(index)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="h-5 w-5 absolute right-2 top-2 cursor-pointer active:opacity-50">
-                                                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z"/>
+                                                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z" />
                                                         </svg>
                                                     </>
                                                 )}
