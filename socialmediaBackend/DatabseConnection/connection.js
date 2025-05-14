e956c249c7e4faee61e31bd4b7797b90e90d5ee5
shubham09anand <shubhamsheelam@gmail.com>
2025-05-14 12:45:18 +0530

    inti

--- socialmediaBackend/DatabseConnection/connection.js
+++ socialmediaBackend/DatabseConnection/connection.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 const connectDB = async () => {
     try {
-        const mongoUrl = 'mongodb://192.168.1.6:27017/SocialMeida';
+        const mongoUrl = 'mongodb+srv://shubham09anand:%40Shubham4963@socialnesst.qpbqi.mongodb.net/?retryWrites=true&w=majority&appName=socialNesst';
         await mongoose.connect(mongoUrl);
         const db = mongoose.connection;
         console.log("Connection successful at server", db.host, "on port", db.port, "to database", db.name);
